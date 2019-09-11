b82eeda435a363cb12909ee2a4c801c126607bb2
Gee-Bee <greg.bunia@gmail.com>
2019-09-11 17:12:06 +0200

    add Procfile for heroku deployment

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.post('/notes', (req, res) => {
   res.status(201).json(note);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
