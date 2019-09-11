c459c36a65de9ab8b02135fdf0a50de1d252ec34
Gee-Bee <greg.bunia@gmail.com>
2019-09-11 18:14:31 +0200

    express static frontend build

---
+++ build/precache-manifest.ad824b517025c95a58c10dbf2edf9d3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b15597eb72c7f40efa4bbf03e69e6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83d4418558f6586dc166",
+    "url": "/static/css/main.f73f7164.chunk.css"
+  },
+  {
+    "revision": "0c093a7de405cae80c4a",
+    "url": "/static/js/2.5d1e4298.chunk.js"
+  },
+  {
+    "revision": "83d4418558f6586dc166",
+    "url": "/static/js/main.10b2f1cd.chunk.js"
+  },
+  {
+    "revision": "09d80c7ac433bfb6de9c",
+    "url": "/static/js/runtime~main.dea9af70.js"
+  }
+]);
