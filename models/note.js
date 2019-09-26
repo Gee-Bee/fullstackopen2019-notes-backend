374b4541af7c8c78095d4e212f1199fd079dd8c5
Grzegorz Bunia <g.bunia@american-systems.pl>
2019-09-26 14:16:57 +0200

    project structure

--- models/note.js
+++ models/note.js
@@ -1,23 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.set('useFindAndModify', false);
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url);
-
-mongoose
-  .connect(url, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  })
-  .then(() => {
-    console.log('connected to MongoDB');
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB', error);
-  });
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
---
+++ utils/middleware.js
@@ -0,0 +1,30 @@
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error.message);
+
+  if ( error.name === 'CastError' && error.kind === 'ObjectId' ) {
+    return res.status(400).json({ error: 'marlformed id' });
+  }
+  if ( error.name === 'ValidationError' ) {
+    return res.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
+}
